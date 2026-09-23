0586db82611d5bc5d3141ab142325b6334ccda7b
孟松 <mengsong@58.com>
2026-09-23 11:08:41 +0800

    模块迁移

--- springbootdemo/src/main/java/org/example/springbootdemo/annonationdemo/AnnotationDemo.java
+++ springbootdemo/src/main/java/org/example/springbootdemo/annonationdemo/AnnotationDemo.java
@@ -1,8 +1,7 @@
 package org.example.springbootdemo.annonationdemo;
 
-import com.example.springbootdemo.annonationdemo.interfacedemo.ILBGSaleCallAICheckService;
+import org.example.springbootdemo.annonationdemo.interfacedemo.ILBGSaleCallAICheckService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
