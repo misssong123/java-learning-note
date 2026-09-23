cf6e5e26c8d5890400c4d2fab018af78dadb350e
孟松 <mengsong@58.com>
2026-09-23 11:45:26 +0800

    新增algo-data-structures模块

---
+++ algo-data-structures/src/main/java/meng/datastructures/tree/BinaryTree.java
@@ -0,0 +1,100 @@
+package meng.datastructures.tree;
+
+public class BinaryTree {
+    private HeroNode root;
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+    //前序遍历
+    public void preOrder() {
+        if(this.root != null) {
+            this.root.preOrder();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    //中序遍历
+    public void infixOrder() {
+        if(this.root != null) {
+            this.root.infixOrder();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+    //后序遍历
+    public void postOrder() {
+        if(this.root != null) {
+            this.root.postOrder();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+    //前序查找
+    public HeroNode preOrderSearch(int no) {
+        if(root != null) {
+            return root.preOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+    //中序查找
+    public HeroNode infixOrderSearch(int no) {
+        if(root != null) {
+            return root.infixOrderSearch(no);
+        }else {
+            return null;
+        }
+    }
+    //后序查找
+    public HeroNode postOrderSearch(int no) {
+        if(root != null) {
+            return this.root.postOrderSearch(no);
+        }else {
+            return null;
+        }
+    }
+    //删除结点
+    public void delNode(int no) {
+        if(root != null) {
+            //如果只有一个root结点, 这里立即判断root是不是就是要删除结点
+            if(root.getNo() == no) {
+                root = null;
+            } else {
+                //递归删除
+                root.delNode(no);
+            }
+        }else{
+            System.out.println("空树，不能删除~");
+        }
+    }
+    public static void main(String[] args) {
+        //先需要创建一颗二叉树
+        BinaryTree binaryTree = new BinaryTree();
+        //创建需要的结点
+        HeroNode root = new HeroNode(1, "宋江");
+        HeroNode node2 = new HeroNode(2, "吴用");
+        HeroNode node3 = new HeroNode(3, "卢俊义");
+        HeroNode node4 = new HeroNode(4, "林冲");
+        HeroNode node5 = new HeroNode(5, "关胜");
+
+        //说明，我们先手动创建该二叉树，后面我们学习递归的方式创建二叉树
+        root.setLeft(node2);
+        root.setRight(node3);
+        node3.setRight(node4);
+        node3.setLeft(node5);
+        binaryTree.setRoot(root);
+
+        //测试
+		System.out.println("前序遍历"); // 1,2,3,5,4
+		binaryTree.preOrder();
+
+        //测试
+		System.out.println("中序遍历");
+		binaryTree.infixOrder(); // 2,1,5,3,4
+
+		System.out.println("后序遍历");
+		binaryTree.postOrder(); // 2,5,4,3,1
+    }
+}
