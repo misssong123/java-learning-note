adf230bf0eb5e8763b96e3ad8754cf7b142db30f
孟松 <mengsong@58.com>
2026-09-23 15:08:05 +0800

    新增book模块

---
+++ bookdemo/src/main/java/com/demo/java_web/chapter003/EncodeWay.java
@@ -0,0 +1,25 @@
+package com.demo.java_web.chapter003;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class EncodeWay {
+    public static void main(String[] args) {
+        String name = "I am 君山";
+        try{
+            System.out.println(Arrays.toString(name.getBytes(StandardCharsets.ISO_8859_1)));
+
+            System.out.println(Arrays.toString(name.getBytes(Charset.forName("GB2312"))));
+
+            System.out.println(Arrays.toString(name.getBytes(Charset.forName("GBK"))));
+
+            System.out.println(Arrays.toString(name.getBytes(StandardCharsets.UTF_16)));
+
+            System.out.println(Arrays.toString(name.getBytes(StandardCharsets.UTF_8)));
+
+        }catch (Exception e){
+
+        }
+    }
+}
